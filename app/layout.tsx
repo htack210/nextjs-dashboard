710c59b33969e281682732bdfb6da0ee580b34c8
htack210 <htack210@live.com>
2024-10-26 16:11:36 -0400

    Made correction to layout.tsx file in app/ui folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}): JSX.Element {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
